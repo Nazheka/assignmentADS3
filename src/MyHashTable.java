eaf2ab34e8bd583ccb613bf258b7105b01dde9c3
Nazheka <akezhanstudy@gmail.com>
2024-05-15 21:13:05 +0500

    implemented fields and constructors for MyHashTable

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,2 +1,29 @@
-public class MyHashTable {
+public class MyHashTable <K, V> {
+    private class HashNode <K, V> {
+        private K key;
+        private V value;
+        private HashNode <K, V> next;
+
+        public HashNode (K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    private MyHashTable(){}
+
+    private MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
 }
