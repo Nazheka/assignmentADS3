efed035dcc0eec49edfd8d8699e70bcae13dce85
Nazheka <akezhanstudy@gmail.com>
2024-05-15 23:30:03 +0500

    implemented fields, constructor, put method for BST

--- src/BST.java
+++ src/BST.java
@@ -1,2 +1,43 @@
-public class BST {
+public class BST <K extends Comparable<K>, V> {
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V val) {
+            this.key = key;
+            this.value = val;
+        }
+    }
+    private Node root;
+    public void put(K key, V value) {
+        Node newNode = new Node(key, value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        Node current = root;
+        Node parent = null;
+
+        while (current != null) {
+            parent = current;
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0) {
+                current = current.left;
+            } else if (cmp > 0) {
+                current = current.right;
+            } else {
+                current.value = value;
+                return;
+            }
+        }
+
+        int cmp = key.compareTo(parent.key);
+        if (cmp < 0) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
+    }
+
 }
